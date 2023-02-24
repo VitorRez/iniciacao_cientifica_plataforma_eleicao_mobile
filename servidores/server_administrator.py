0c70d39f4d2859062106482d0a25eea9333db5f6
VitorRez <vitorez2002@gmail.com>
2023-02-24 12:25:58 -0300

    servidor do administrador concluido

--- servidores/server_administrator.py
+++ servidores/server_administrator.py
@@ -26,13 +26,13 @@ def handle_client(conn, addr, adm):
             print(f"[{addr}] {msg}")
             dados = msg.split()
             if dados[0] == "CANDIDATAR":
-                print()
+                candidatar(conn, addr, adm)
                 #metodo para registrar
             if dados[0] == "APOIAR CANDIDATO":
-                print()
-                #metodo para apoiar candidato
+                apoiar(conn, addr, adm)
     
 def candidatar(conn, addr, adm):
+    print("[O CLIENTE IRÁ SE INSCREVER COMO CANDIDATO]")
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -46,5 +46,20 @@ def candidatar(conn, addr, adm):
             adm.candidato_a_candidato(dados[0], dados[1], dados[2])
             conn.send("Candidadura enviada, esperando aprovação!".encode(FORMAT))
 
-#def apoiar(conn, addr, adm)
+def apoiar(conn, addr, adm):
+    print("[O CLIENTE IRÁ APOIAR A CANDIDATURA DE OUTRO CLIENTE]")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            dados = msg
+            print(f"[{addr}] {msg}")
+            dados = msg.split()
+            adm.registra_candidato(dados[0], dados[1], dados[2], dados[3], dados[4], dados[5])
+
 
