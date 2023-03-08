af15c5a671d3bbe82b88ea14958698798aa16758
VitorRez <vitorez2002@gmail.com>
2023-03-08 10:56:17 -0300

    adicionados metodos para cifra assimétrica

--- crypto/encryptDecrypt.py
+++ crypto/encryptDecrypt.py
@@ -1,9 +1,23 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 class Encryptor:
     def __init__(self, key):
         self.key = key
+
+    def encrypt_sym(self, message):
+        cipher = AES.new(self.aes_key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(message.encode('utf-8'))
+        return (nonce, ciphertext, tag)
+
+    def decrypt_sym(self, nonce, ciphertext, tag, key):
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+        message = cipher.decrypt(ciphertext)
+        message = bytes.decode(message)
+        return message
 
     def encrypt_pub(self, message):
         cipher = PKCS1_OAEP.new(self.key.public_key())
